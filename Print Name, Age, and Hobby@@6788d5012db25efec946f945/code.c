9c0ec622828ca062da2dac78b5243bea4fe73feb
Rohit choudhary <rohitchoudhary0425@gmail.com>
2025-02-10 19:17:18 +0530

    Solution to Print Name, Age, and Hobby

--- Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
+++ Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+
 int main() {
     int a;
-    scanf("%d",&a);
+    scanf("%d", &a);
+    getchar(); // Consume the newline character
+
     char b[20];
-    scanf("%19s",b);
+    scanf("%19s", b); // Leave space for the null terminator
+
+    getchar(); // Consume the newline character
+
     char c[40];
-    scanf("%39s",c);
-    printf("Name: %s\n",b);
-     printf("Age: %d\n",a);
-      printf("Hobby: %s\n", c);
-    return 0;
+    scanf("%39s", c); // Leave space for the null terminator
+
+    printf("Name: %s\n", b);
+    printf("Age: %d\n", a);
+    printf("Hobby: %s\n", c);
+
+    return 0;
 }
