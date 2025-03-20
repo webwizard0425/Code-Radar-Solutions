752b0d100c942b616950f4faaeeb05d882eecc8f
Rohit choudhary <rohitchoudhary0425@gmail.com>
2025-03-20 21:09:11 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,5 +1,9 @@
 void fibonacciSeries(int a){
-    if(a==1) printf("%d",1);
+    if(a==1) {
+        printf("%d",1);
+        return;
+    }
     int b=fibonacciSeries(a-1)+fibonacciSeries(a-2);
     printf("%d",b);
+    return;
 }
