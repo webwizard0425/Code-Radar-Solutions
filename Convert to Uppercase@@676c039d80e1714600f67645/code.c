008049b3ad1b977c92b14de9022716dbbaa5638b
Rohit choudhary <rohitchoudhary0425@gmail.com>
2025-04-04 15:06:52 +0530

    Solution to Convert to Uppercase

--- Convert to Uppercase@@676c039d80e1714600f67645/code.c
+++ Convert to Uppercase@@676c039d80e1714600f67645/code.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(){
+int main() {
     char str[100];
-    fgets(str, sizeof(str) , stdin);
+    fgets(str, sizeof(str), stdin);
     int n = strlen(str);
 
-    for(int i = 0 ; i<n ; i++){
-      char ch = str[i]+32;
-      printf("%c" , ch);
+    for (int i = 0; i < n; i++) {
+        char ch = tolower(str[i]);
+        printf("%c", ch);
     }
+
+    return 0;
 }
